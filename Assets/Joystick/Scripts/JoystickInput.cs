93624ff4dad1643071cd4de97c9c04f90b641ffc
Aptypp <artur.kucherenko@inkos.games>
2022-07-09 14:14:39 +0300

    Fixed screen size dependency for joystick input

--- Assets/Joystick/Scripts/JoystickInput.cs
+++ Assets/Joystick/Scripts/JoystickInput.cs
@@ -5,18 +5,29 @@ namespace AVP.Joystick.Scripts
 {
     public abstract class JoystickInput : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
     {
+        [Header("References")]
         [SerializeField] private JoystickView _joystickView;
 
         [Header("Settings")]
-        [SerializeField] private float _radius = 100.0f;
+        [SerializeField] private float _referenceRadius = 100.0f;
+        [SerializeField] private Vector2 _referenceResolution = new (1920, 1080);
 
         protected bool _isActive;
         protected Vector2 _direction;
+        
+        private float _radius;
+        private float _inverseRadius;
 
+        private void Awake() => InitFields();
+        
+#if UNITY_EDITOR
+        private void OnValidate() => InitFields();
+#endif
+        
         public virtual void OnPointerDown(PointerEventData eventData)
         {
             _isActive = true;
-            _direction = eventData.position - (Vector2) _joystickView.getBackImageTransform.position;
+            _direction = (eventData.position - (Vector2) _joystickView.getBackImageTransform.position).normalized;
             SetJoystickFrontImagePosition(eventData.position);
         }
 
@@ -26,7 +37,7 @@ namespace AVP.Joystick.Scripts
             
             SetJoystickFrontImagePosition((Vector2)_joystickView.getBackImageTransform.position + newDirection);
             
-            _direction = newDirection;
+            _direction = newDirection * _inverseRadius;
         }
 
         public virtual void OnPointerUp(PointerEventData eventData)
@@ -37,5 +48,11 @@ namespace AVP.Joystick.Scripts
         }
 
         private void SetJoystickFrontImagePosition(Vector2 position) => _joystickView.getFrontImageTransform.position = position;
+
+        private void InitFields()
+        {
+            _radius = _referenceRadius * Screen.width / _referenceResolution.x;
+            _inverseRadius = 1.0f / _radius;
+        }
     }
 }
